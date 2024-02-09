b4efae70c5dc7a964fc01c792b8d763597660211
Dhruv Mehta <flashxx001@gmail.com>
2024-02-09 22:23:40 +0530

    Basics of Javascript. Way more to gooo...

---
+++ Javascript/Vasiables and Constants/script.js
@@ -0,0 +1,31 @@
+// Variables and Constants are used to store data, and in variables, the data is stored and can be changed. But in constants data can't be store and changed.
+
+var a = 12;
+var b = 22;
+const c = 32;
+
+
+// Compilers and Interpreters
+
+// JS Understands English but computer doesn't understands english.
+
+// Console Alert and Prompt
+
+// console.lg
+console.log("Hello");
+
+//console.warn();
+console.warn("Dhruv Mehta");
+
+
+// console.error();
+console.error("Dhruv Mehta");
+
+
+// Alert
+
+alert("Make sure this is correct");
+
+
+// Prompt
+prompt("Yes, this is Correct");
